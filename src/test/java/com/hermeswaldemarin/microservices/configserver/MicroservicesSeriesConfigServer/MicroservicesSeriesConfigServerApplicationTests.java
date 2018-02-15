66a7c768f1d77ef98282f839ef05e227aeb9cf2a
Hermes Waldemarin Neto <hermesneto@plusoft.com.br>
2018-02-15 08:07:05 -0200

    Inicio do Projeto Microservices Series. Episodio 2

---
+++ src/test/java/com/hermeswaldemarin/microservices/configserver/MicroservicesSeriesConfigServer/MicroservicesSeriesConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hermeswaldemarin.microservices.configserver.MicroservicesSeriesConfigServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroservicesSeriesConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
